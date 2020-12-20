7abd30025d088bdfea90d89f8f0b114cd037ec4d
Avirut Mehta <avirut.mehta@gmail.com>
2020-12-20 14:53:58 -0600

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,3 @@
-document.querySelector('html').classList.add('__fb-dark-mode')
+document.querySelector('html').classList.add('__fb-dark-mode');
+
+setTimeout(function() {document.querySelector('html').classList.add('__fb-dark-mode');}, 500);
